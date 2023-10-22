1db10f12aeec722d99242e27e9ac88ca2d118974
Marcos Vitor <marcosvictor88@gmail.com>
2023-10-21 23:17:27 -0300

    Botao de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -15,11 +16,26 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public void cadastrarProduto (){
         
         
-        //conn = new conectaDAO().connectDB();
-        
+        conn = new conectaDAO().connectDB();
+        ProdutosDTO produto = new ProdutosDTO();
+        PreparedStatement ps;
+        String sql = "insert into produtosL values (?,?,?)";
+        try {
+            ps = (PreparedStatement) conn.prepareStatement(sql);
+            ps.setString(1,produto.getNome());
+            ps.setDouble(2,produto.getValor());
+            ps.setString(3,produto.getStatus());
+
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null," Cadastro do produto realizado !");
+        } catch (SQLException sqle) {
+          JOptionPane.showMessageDialog(null," erro ao cadastrar!"+sqle.getMessage());
+
+        }
+
         
     }
     
---
+++ src/inicializar.java
@@ -0,0 +1,9 @@
+
+public class inicializar {
+    
+    public static void main (String[] args){
+        
+         cadastroVIEW CAD = new cadastroVIEW();
+        CAD.setVisible(true);
+    }
+}
